be37999542046745c301a81795620b0fa8ec749f
Alba Gustems <albagustems@gmail.com>
2021-01-03 21:08:51 +0100

    Creating the HTML for the products and editproduct components

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Products = () => {
     return (
-        <div>
-            From Products.js
-        </div>
+        <>
+            <h2 className="text-center my-5"> Product Listing</h2>
+            <table className="table table-striped">
+                <thead className="bg-primary table-dark">
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </>
     )
 }
 
