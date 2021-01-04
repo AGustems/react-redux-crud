057efc73269fc39357d247a8e78f23b153ddbd9b
Alba Gustems <albagustems@gmail.com>
2021-01-04 20:24:41 +0100

    Configure the redux enviroment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
+// Redux
+import {Provider} from 'react-redux'
+import store from './store'
+
 import Header from './components/Header'
 import Products from './components/Products'
 import NewProduct from './components/NewProduct'
@@ -9,14 +13,16 @@ import EditProduct from './components/EditProduct'
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <div className="container mt-5">
-        <Switch>
-          <Route exact path="/" component={Products} />
-          <Route exact path="/products/new" component={NewProduct} />
-          <Route exact path="/products/edit/:id" component={EditProduct} />
-        </Switch>
-      </div>
+      <Provider store={store}>
+        <Header />
+        <div className="container mt-5">
+          <Switch>
+            <Route exact path="/" component={Products} />
+            <Route exact path="/products/new" component={NewProduct} />
+            <Route exact path="/products/edit/:id" component={EditProduct} />
+          </Switch>
+        </div>
+      </Provider>
     </BrowserRouter>
   );
 }
---
+++ src/reducers/productsReducer.js
@@ -0,0 +1,15 @@
+// Each reducer has it's own state
+const initialState = {
+    products: [],
+    error: false,
+    loading: false,
+}
+
+export default function(state = initialState, action) {
+    switch(action.type){
+        
+        
+        default:
+            return state
+    }
+}
