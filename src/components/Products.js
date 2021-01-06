d8a6a42b0c0def228e0f5eab4c9b59f0279be518
Alba Gustems <albagustems@gmail.com>
2021-01-06 15:56:08 +0100

    Showing the error and loading states in Products.js component

--- src/components/Products.js
+++ src/components/Products.js
@@ -15,10 +15,16 @@ const Products = () => {
 
     // Obtain the state
     const products = useSelector(state => state.products.products)
-    
+    const error = useSelector(state => state.products.error)
+    const loading = useSelector (state => state.products.loading)
+
     return (
         <>
             <h2 className="text-center my-5"> Product Listing</h2>
+            {error ? <p className="font-weight-bold alert alert-danger text-center mt-4">
+                There was an error. Please try again</p> : null}
+
+            {loading ? <p className="text-center"> Loading... </p>: null}
             <table className="table table-striped">
                 <thead className="bg-primary table-dark">
                     <tr>
